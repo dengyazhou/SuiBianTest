0b88fb38c520984b0929abffb3dedebc2638d7b1
dengyazhou <mac@MacdeiMac-10.local>
2016-03-12 14:59:19 -0800

    Summary123
    123

---
+++ TestDemo/TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by Mac on 16/3/12.
+//  Copyright © 2016年 Syxdzybs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
